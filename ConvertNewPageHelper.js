be182fb56122b61c63e3299625539d5304a236eb
Tianw22 <33138468+Tianw22@users.noreply.github.com>
2020-03-04 22:33:36 -0500

    Create ConvertNewPageHelper.js

---
+++ ConvertNewPageHelper.js
@@ -0,0 +1,32 @@
+({
+    update : function(component, event, helper){
+       component.find("editForm").submit(); 
+        
+    },
+
+    ConvertIt : function(component, event, helper) {  
+        var action = component.get("c.convertIt");
+        action.setParams({"recordId": component.get("v.recordId")});
+        action.setCallback(this, function(res) {
+        var response = res.getReturnValue();
+        var state = action.getState();
+        console.log(response);
+        if(component.isValid() && state == "SUCCESS"){            
+            var toastEvent = $A.get("e.force:showToast");
+            toastEvent.setParams({
+                "title": "Success!",
+                "message": "This Lead has been converted.",
+                "type ": "success"
+            });
+
+            toastEvent.fire(); 
+            $A.get("e.force:refreshView").fire();
+            location.reload();
+            
+         } else if (state == "ERROR") {
+            console.log('There was a problem and the state is: '+ action.getState());
+         }
+      });
+      $A.enqueueAction(action); 
+    }
+})
