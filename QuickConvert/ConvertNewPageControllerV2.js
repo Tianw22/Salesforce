b3b11e4c2d08c3e88045bb3ca239674a87550bc6
Tianw22 <33138468+Tianw22@users.noreply.github.com>
2020-04-25 16:35:32 -0400

    Update ConvertNewPageControllerV2.js

--- QuickConvert/ConvertNewPageControllerV2.js
+++ QuickConvert/ConvertNewPageControllerV2.js
@@ -1,27 +1,5 @@
-({
-    doInit: function(cmp, event, helper) {
-         var getFields = cmp.get('c.getFieldSetMember');
-         getFields.setParams({
-                     "objectName" :  "Lead",
-                     "fieldSetName" :  "LeadConversionFieldSet"
-         });
-         getFields.setCallback(this, function(response){
-             var state = response.getState();
-             if(state === 'SUCCESS'){
-                 var fields = response.getReturnValue();
-                 console.log('fields',fields);
-                 cmp.set('v.fields',fields);
-                    
-                 }else if(state==='INCOMPLETE'){
-                 }else if(state ==='ERROR'){
-                     console.log('An error occurred.');
-                 }
-            });
-         
-         $A.enqueueAction(getFields);	
-    },
-    
-    SC : function(component, event, helper){
+({  
+    SC : function(component, event, helper){        
         var invalidFields = helper.isFormValid(component);
         if(invalidFields.length > 0){
             var toastEvent2 = $A.get("e.force:showToast");
