793097800bf3205eb8f34b5d60d1861029b2a434
Tianw22 <33138468+Tianw22@users.noreply.github.com>
2020-04-27 19:52:28 -0400

    Update and rename HSActivityToSF/Email/Procedure1.py to HSActivityToSF/Email&Call/Email-Procedure1.py

--- HSActivityToSF/Email&Call/Email-Procedure1.py
+++ HSActivityToSF/Email&Call/Email-Procedure1.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
-dfall = pd.read_excel('hubspot-crm-exports-all-emails-with-contact.xlsx',encoding='utf-8-sig')
+dfall = pd.read_excel('System/hubspot-crm-exports-all-emails-with-contact-2020-04-27.xlsx',encoding='utf-8-sig')#,encoding='utf8')
 dfall = pd.DataFrame(dfall)
+obj = "EMAIL"
 
 dfall_rename = dfall.rename(columns={'Engagement ID': 'HubSpot Activity ID',
                                      'Activity type': 'TYPE',
@@ -11,7 +12,7 @@ dfall_rename = dfall.rename(columns={'Engagement ID': 'HubSpot Activity ID',
                                      'HubSpot Team': 'HubSpot Team (Excel)',
                                      'Email body': 'DESCRIPTION',
                                      'Contact ID': 'WHOID'}) 
-                                     
+
 dfall_rename['COMPLETEDDATTIME'] = list(dfall_rename['ACTIVITYDATE'])
 dfall_rename['CREATE DATE'] = list(dfall_rename['ACTIVITYDATE'])
 dfall_rename['Owner Name (Excel)'] = list(dfall_rename['CREATEDBYID'])
@@ -21,12 +22,12 @@ dfall_drop = dfall_rename.drop(columns=["Activity created by","Activity date","E
 dfall_drop["TYPE"].replace({"Email sent to contact":"Email",
                             "Email reply from contact":"Replied Email"},
                             inplace=True)
-                        
+
 dfall_drop['TASKSUBTYPE'] = ["Email"]*len(dfall_drop) 
 dfall_drop['STATUS'] = ["Completed"]*len(dfall_drop) 
-dfall_drop['RECORDTYPEID'] = ["xxxxxxxxxxxxxxx"]*len(dfall_drop) #Find it in Salesforce data loader export
+dfall_drop['RECORDTYPEID'] = ["xxxxxxxxxxxxxxxxx"]*len(dfall_drop) #Find the ID from data loader export
 
-user = pd.read_excel('User.xlsx',encoding='utf-8-sig')
+user = pd.read_excel('System/User.xlsx',encoding='utf-8-sig')
 user = pd.DataFrame(user)
 userid = user['Id']
 username = user['HS User']
@@ -34,13 +35,13 @@ for i in range(0,len(user)):
     n1 = userid[i]
     n2 = username[i]
     dfall_drop["CREATEDBYID"].replace({n2:n1},inplace=True)
-
+    
 dfall_drop['OWNERID'] = list(dfall_drop['CREATEDBYID'])
 
 dfcon = dfall_drop[dfall_drop['Salesforce target object'] == 'Contact']
-dfcon.to_excel("emailconraw.xlsx",index = False)
+dfcon.to_excel("OutPut/%sconraw.xlsx"%obj,index = False)
 len(dfcon)
 
 dfld = dfall_drop[dfall_drop['Salesforce target object'] == 'Lead']
-dfld.to_excel("emailldraw.xlsx",index = False)
+dfld.to_excel("OutPut/%sldraw.xlsx"%obj,index = False)
 len(dfld)
