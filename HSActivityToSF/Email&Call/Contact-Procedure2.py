dda3fdc6392739ca1190f50a2ad0c242c3b8448c
Tianw22 <33138468+Tianw22@users.noreply.github.com>
2020-04-27 19:55:37 -0400

    Update and rename HSActivityToSF/Email/Contact-Procedure2.py to HSActivityToSF/Email&Call/Contact-Procedure2.py

--- HSActivityToSF/Email&Call/Contact-Procedure2.py
+++ HSActivityToSF/Email&Call/Contact-Procedure2.py
@@ -2,15 +2,15 @@ import pandas as pd
 from datetime import datetime
 import time
 
-dfcon = pd.read_excel('emailconraw.xlsx',encoding='utf-8-sig')#,encoding='utf8')
+obj = input().upper() #email or call
+
+dfcon = pd.read_excel('OutPut/%sconraw.xlsx'%obj,encoding='utf-8-sig')#,encoding='utf8')
 dfcon = pd.DataFrame(dfcon)
 
-con = pd.read_csv('con-id-4-26-extract.csv')
+con = pd.read_csv('System/con-id-extract.csv')
 con = pd.DataFrame(con)
-con.columns
 
 con = con.rename(columns={"HUBSPOT_CONTACT_ID_HUBSPOT_INTEGRATION__C":"WHOID"})
-con.columns
 
 con = con.dropna().reset_index(drop=True)
 
@@ -21,6 +21,7 @@ dfcon['CONTACTID'] = dfcon.WHOID.map(conidmap)
 
 dfcon = dfcon.drop(columns=["WHOID","Salesforce target object"])
 dfconnew = dfcon.rename(columns={"CONTACTID":"WHOID"})
+dfconnew = dfconnew.dropna(subset=['WHOID']).reset_index().drop(columns = ['index'])
 
 actdatelist = list(dfconnew['ACTIVITYDATE'].astype(str))
 for i in range(0,len(actdatelist)):
@@ -31,11 +32,12 @@ for i in range(0,len(actdatelist)):
 dfconnew['ACTIVITYDATE'] = actdatelist
 
 dfconidmap = dfconnew[["HubSpot Activity ID","WHOID"]]
-dfconidmap.to_excel("ContactIdActIdMap.xlsx",index = False)
+dfconidmap.to_excel("IDMAP/ContactIdActIdMap-%s.xlsx"%obj,index = False)
 
 dfcondrop = dfconnew.drop(columns=["WHOID"])
 
 dfconnew2 = dfcondrop.drop_duplicates(subset=['HubSpot Activity ID'], keep=False).reset_index().drop(columns = ['index'])
+
 final = dfconnew2[~dfconnew2.OWNERID.str.contains("(Deactivated)")].reset_index().drop(columns = ['index'])
 
-final.to_csv("EMAIL-contact-NoConID-for-import.csv", index=False, encoding='utf-8-sig')
+final.to_csv("OutPut/%s-contact-NoConID.csv"%obj, index=False, encoding='utf-8-sig')
