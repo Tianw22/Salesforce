cdc93c3c877993f8e0a27aedfbee083f1a2808e6
Tianw22 <33138468+Tianw22@users.noreply.github.com>
2020-04-27 19:58:09 -0400

    Update and rename HSActivityToSF/Email/Lead-Procedure2.py to HSActivityToSF/Email&Call/Lead-Procedure2.py

--- HSActivityToSF/Email&Call/Lead-Procedure2.py
+++ HSActivityToSF/Email&Call/Lead-Procedure2.py
@@ -2,24 +2,28 @@ import pandas as pd
 from datetime import datetime
 import time
 
-dfld = pd.read_excel('emailldraw.xlsx',encoding='utf-8-sig')#,encoding='utf8')
+obj = input().upper() #email or call
+
+dfld = pd.read_excel('OutPut/%sldraw.xlsx'%obj,encoding='utf-8-sig')#,encoding='utf8')
 dfld = pd.DataFrame(dfld)
 
-ld = pd.read_csv('lead-id-extract.csv')
+ld = pd.read_csv('System/lead-id-extract.csv')
 ld = pd.DataFrame(ld)
 
 ld = ld.rename(columns={"HUBSPOT_CONTACT_ID_HUBSPOT_INTEGRATION__C":"WHOID"})
+
 ld = ld.dropna().reset_index(drop=True)
+
 ld["WHOID"] = ld["WHOID"].astype(int)
 
 ldidmap = ld.set_index("WHOID").ID
-dfld['LEADID'] = dfld.WHOID.map(ldidmap) ### REPLACE HUBSPOT ID WITH SALESFORCE ID
+dfld['LEADID'] = dfld.WHOID.map(ldidmap)
 
 dfld = dfld.drop(columns=["WHOID","Salesforce target object"])
 dfldnew = dfld.rename(columns={"LEADID":"WHOID"})
+dfldnew = dfldnew.dropna(subset=['WHOID']).reset_index().drop(columns = ['index'])
 
 actdatelist = list(dfldnew['ACTIVITYDATE'].astype(str))
-
 for i in range(0,len(actdatelist)):
     inDate = actdatelist[i]
     d = datetime.strptime(inDate,"%Y-%m-%d %H:%M:%S")
@@ -29,4 +33,4 @@ dfldnew['ACTIVITYDATE'] = actdatelist
 
 final = dfldnew[~dfldnew.OWNERID.str.contains("(Deactivated)")].reset_index().drop(columns = ['index'])
 
-final.to_csv("EMAIL-lead-for-import.csv", index=False, encoding='utf-8-sig')
+final.to_csv("OutPut/%s-lead-for-import.csv"%obj, index=False, encoding='utf-8-sig')
